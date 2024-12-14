93945c2167b1d75f4659222b83ed5f9a4be21069
DYung26 <theadstore1407@gmail.com>
2024-12-14 05:24:28 +0100

    Add documentation and cleanup comments in `routes/index.js`.

--- routes/index.js
+++ routes/index.js
@@ -3,11 +3,11 @@
  * It sets up the endpoints for 'status' and 'stats' using the methods
  * from the `AppController` to handle the respective requests.
  */
-import AppController from "../controllers/AppController";
+import AppController from '../controllers/AppController';
 
 const router = (api) => {
   api.get('/status', AppController.getStatus);
   api.get('/stats', AppController.getStats);
-}
+};
 
 export default router;
